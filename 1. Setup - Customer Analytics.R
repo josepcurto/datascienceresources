73310bcd9e3112ad7268e10916da0071329dc5ea
Josep Curto <josep.curto@gmail.com>
2014-12-11 15:01:12 +0000

    Initial Commit

---
+++ 1. Setup - Customer Analytics.R
@@ -0,0 +1,12 @@
+###############################################################################
+## Title: Data Science - Environment Setup
+## Version: 0.1
+## Autor: Josep Curto
+## Description: Packages that can be used for Data Science
+###############################################################################
+
+# Package installation
+install.packages(“clusterCrit”)
+
+# Package load
+library(clusterCrit) #Clustering indices
